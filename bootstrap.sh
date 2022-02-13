ea4f89e81bcf6bf7e7816103db62a2c09fcab60c
hugo tse <hugotse@theorigo.com>
2022-02-13 14:34:48 +0000

    Add sudo in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
-yum install -y python3
+sudo yum install -y python3
 sudo -u centos pip3 install aws-sam-cli --user
-yum install -y git
-yum install -y zip unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+sudo yum install -y git
+sudo yum install -y zip unzip
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
 cd awscliv2
-./aws/install
-yum install -y yum-utils
-yum-config-manager \
+sudo ./aws/install
+sudo yum install -y yum-utils
+sudo yum-config-manager \
 --add-repo \
 https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io
-systemctl start docker
-usermod -a -G docker centos
-git config --global user.name "Hugo Tse"
-git config --global user.email hugotkk@live.hk
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo usermod -a -G docker centos
+sudo -u centos git config --global user.name "Hugo Tse"
+sudo -u centos git config --global user.email hugotkk@live.hk
